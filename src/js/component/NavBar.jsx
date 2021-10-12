4039b40feac9fae2d0a350321d55c4b6b0dfa88a
nrob92 <nrob42@gmail.com>
2021-10-12 02:04:28 +0000

    react app

---
+++ src/js/component/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function NavBar() {
+	return (
+		<div className="nav">
+			<p className="nav-left">hello</p>
+			<ul className="nav-right">
+				<li>words</li>
+				<li>word</li>
+				<li>worddd</li>
+			</ul>
+		</div>
+	);
+}
+
+export default NavBar;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,17 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import Card from "./Card";
+import Jumbo from "./Jumbo.jsx";
+import NavBar from "./NavBar.jsx";
+import Section from "./Section.jsx";
 
-//create your first component
 const Home = () => {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<NavBar />
+			<Section />
+			<Jumbo />
+			<Card />
 		</div>
 	);
 };
